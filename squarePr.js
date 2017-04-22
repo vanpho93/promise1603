c1b13fe95a5b660365bf2a2c3c2574b770a5c8aa
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-22 21:14:24 +0700

    ket thuc

---
+++ squarePr.js
@@ -0,0 +1,18 @@
+const { mulPromise, addPromise, divPromise } = require('./addPr');
+
+// 3, 4, 5
+function getSquare(a, b, h) {
+    return addPromise(a, b)
+    .then(ab => mulPromise(ab, h))
+    .then(abh => divPromise(abh, 2));
+}
+
+async function getSquareAsync (a, b, h) { // eslint-disable-line
+    const ab = await addPromise(a, b);
+    const abh = await mulPromise(ab, h);
+    const square = await divPromise(abh, 2);
+    return square;
+}
+
+getSquareAsync(3, 4, 5)
+.then(a => console.log(a));
